b8e71802ca9088e42d6ef972776a8d853c947712
saulpenagamero <sapenagamero@gmail.com>
2020-07-13 22:57:17 -0400

    PinchDetectionScript

---
+++ Assets/HandTrackingWorkshop/Scripts/HTPinchDetection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HTPinchDetection : MonoBehaviour
+{
+    public OVRHand handToTrackPinching;
+    public bool pinchThumb;
+    public float pinchThumbForce;
+    public bool pinchIndex;
+    public float pinchIndexForce;
+    public bool pinchMiddle;
+    public float pinchMiddleForce;
+    public bool pinchRing;
+    public float pinchRingForce;
+    public bool pinchPinky;
+    public float pinchPinkyForce;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (handToTrackPinching != null)
+        {
+            pinchThumb = handToTrackPinching.GetFingerIsPinching(OVRHand.HandFinger.Thumb);
+            pinchIndex = handToTrackPinching.GetFingerIsPinching(OVRHand.HandFinger.Index);
+            pinchMiddle = handToTrackPinching.GetFingerIsPinching(OVRHand.HandFinger.Middle);
+            pinchRing = handToTrackPinching.GetFingerIsPinching(OVRHand.HandFinger.Ring);
+            pinchPinky = handToTrackPinching.GetFingerIsPinching(OVRHand.HandFinger.Pinky);
+
+            pinchThumbForce = handToTrackPinching.GetFingerPinchStrength(OVRHand.HandFinger.Thumb);
+            pinchIndexForce = handToTrackPinching.GetFingerPinchStrength(OVRHand.HandFinger.Index);
+            pinchMiddleForce = handToTrackPinching.GetFingerPinchStrength(OVRHand.HandFinger.Middle);
+            pinchRingForce = handToTrackPinching.GetFingerPinchStrength(OVRHand.HandFinger.Ring);
+            pinchPinkyForce = handToTrackPinching.GetFingerPinchStrength(OVRHand.HandFinger.Pinky);
+        }
+    }
+}
