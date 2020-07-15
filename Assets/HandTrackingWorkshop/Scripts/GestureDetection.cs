7b0e3948a27b981cecb03192c3a4367df7e8e836
saulpenagamero <sapenagamero@gmail.com>
2020-07-14 23:52:42 -0400

    GestureDetection Scene

---
+++ Assets/HandTrackingWorkshop/Scripts/GestureDetection.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public struct Gesture
+{
+    public string name;
+    public List<Vector3> fingerDatas;
+    public UnityEvent onRecognized;
+}
+
+
+public class GestureDetection : MonoBehaviour
+{
+    public OVRSkeleton skeleton;
+    public bool debugMode = false;
+    
+    public List<Gesture> gestures;
+    public List<OVRBone> fingerBones;
+    public float threshold = 0.1f;
+    private Gesture previousGesture;
+    public Gesture _currentGesture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousGesture = new Gesture();
+        _currentGesture = new Gesture();
+    }
+
+    public void setFingerBones()
+    {
+        if (skeleton != null)
+        {
+            if (skeleton.Bones.Count != 0)
+            {
+                Debug.Log("SettingFingerBones");
+                fingerBones = new List<OVRBone>(skeleton.Bones);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fingerBones == null)
+        {
+            setFingerBones();
+        }
+
+        if (debugMode && Input.GetKeyDown(KeyCode.Space))
+        {
+            Save();
+        }
+
+        _currentGesture = Recognize();
+        bool hasRecognized = !_currentGesture.Equals(new Gesture());
+
+        if(hasRecognized && !_currentGesture.Equals(previousGesture))
+        {
+            Debug.Log("New Gesture Found  "+ _currentGesture.name);
+            previousGesture = _currentGesture;
+            _currentGesture.onRecognized.Invoke();
+        }
+    }
+
+    void Save()
+    {
+        Gesture g = new Gesture();
+        g.name = "New Gesture";
+        List<Vector3> data = new List<Vector3>();
+        foreach (var bone in fingerBones)
+        {
+            data.Add(skeleton.transform.InverseTransformPoint(bone.Transform.position));
+        }
+        g.fingerDatas = data;
+        gestures.Add(g);
+    }
+
+    Gesture Recognize()
+    {
+        Gesture currentgesture = new Gesture();
+        float currentMin = Mathf.Infinity;
+
+        foreach (var gesture in gestures)
+        {
+            float sumDistance = 0;
+            bool isDiscarded = false;
+            for (int i = 0; i < fingerBones.Count; i++)
+            {
+                Vector3 currentData = skeleton.transform.InverseTransformPoint(fingerBones[i].Transform.position);
+                float distance = Vector3.Distance(currentData, gesture.fingerDatas[i]);
+                if (distance > threshold)
+                {
+                    isDiscarded = true;
+                    break;
+                }
+                sumDistance += distance;
+            }
+
+            if (!isDiscarded && sumDistance < currentMin)
+            {
+                currentMin = sumDistance;
+                currentgesture = gesture;
+            }
+        }
+        return currentgesture;
+    }
+
+
+}
---
+++ Assets/HandTrackingWorkshop/Scripts/HTGestureDisplayDemo.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HTGestureDisplayDemo : MonoBehaviour
+{
+    public Image leftThumbsUp;
+    public Image rightThumbsUp;
+    public Image leftOpenPalm;
+    public Image rightOpenPalm;
+    public Image leftPeaceSign;
+    public Image rightPeaceSign;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void turnButtonOn(Image imageToTurnOn)
+    {
+        imageToTurnOn.color = Color.red;
+    }
+
+    public void turnButtonOff(Image imageToTurnOn)
+    {
+        imageToTurnOn.color = Color.white;
+    }
+
+    public void turnOffAllLeftImages()
+    {
+        turnButtonOff(leftThumbsUp);
+        turnButtonOff(leftOpenPalm);
+        turnButtonOff(leftPeaceSign);
+    }
+
+    public void turnOffAllRightImages()
+    {
+        turnButtonOff(rightThumbsUp);
+        turnButtonOff(rightOpenPalm);
+        turnButtonOff(rightPeaceSign);
+    }
+
+    public void gestureFoundRight(Image imageToTurnOn)
+    {
+        turnOffAllRightImages();
+        turnButtonOn(imageToTurnOn);
+    }
+
+    public void gestureFoundLeft(Image imageToTurnOn)
+    {
+        turnOffAllLeftImages();
+        turnButtonOn(imageToTurnOn);
+    }
+}
