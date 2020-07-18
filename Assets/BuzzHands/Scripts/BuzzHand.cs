1abb8ee777ac80b635c660e42a338053686ff16d
Howard Olah-Reiken <howard@preferredapp.com>
2020-07-18 18:40:58 -0400

    added hands and bones

---
+++ Assets/BuzzHands/Scripts/BuzzHand.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuzzHand : MonoBehaviour
+{
+    public GameObject spherePrefab;
+
+    private OVRHand oVRHand;
+    public OVRHand OVRHand
+    {
+        get
+        {
+            return oVRHand;
+        }
+        set
+        {
+            oVRHand = value;
+        }
+    }
+
+    private IList<OVRBone> bones;
+
+    private Transform thumbTip;
+    private Transform indexTip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowBones());
+    }
+
+
+    // Update is called once per frame
+    void xUpdate()
+    {
+
+    }
+
+    IEnumerator ShowBones()
+    {
+        yield return new WaitForSeconds(2);
+        while (bones == null || bones.Count == 0)
+        {
+            if (oVRHand != null)
+            {
+                OVRSkeleton oVRSkeleton = oVRHand.GetComponent<OVRSkeleton>();
+                if (oVRSkeleton != null)
+                {
+                    bones = oVRSkeleton.Bones;
+                    if (bones.Count > (int)OVRSkeleton.BoneId.Hand_MaxSkinnable)
+                    {
+                        thumbTip = bones[(int)OVRSkeleton.BoneId.Hand_ThumbTip].Transform;
+                        indexTip = bones[(int)OVRSkeleton.BoneId.Hand_IndexTip].Transform;
+
+                        int boneIndex;
+                        for (boneIndex = 0; boneIndex < bones.Count; boneIndex++)
+                        {
+                            OVRBone bone = bones[boneIndex];
+                            GameObject sphere = GameObject.Instantiate(spherePrefab, bone.Transform);
+                            if (boneIndex >= (int)OVRSkeleton.BoneId.Hand_MaxSkinnable)
+                            {
+                                sphere.GetComponent<Renderer>().material.color = Color.blue;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/BuzzHands/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public OVRHand leftOVRHand;
+    public OVRHand rightOVRHand;
+
+    public BuzzHand buzzHandPrefab;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+        }
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BuzzHand leftBuzzHand = BuzzHand.Instantiate(buzzHandPrefab);
+        leftBuzzHand.OVRHand = leftOVRHand;
+        BuzzHand rightBuzzHand = BuzzHand.Instantiate(buzzHandPrefab);
+        rightBuzzHand.OVRHand = rightOVRHand;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
